d1d7c87f263fb02d363960af567f460b83f9dad8
Daffa Rizqy <143333735+MemoriesEndX@users.noreply.github.com>
2023-08-27 23:18:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+// REQUEST ADALAH DATA MASUK DARI LUAR
+// RESPON ADALAH DATA KELUAR DARI SISTEM
+
+http.createServer(function (req, res) {
+    response.end('HELLO BRO');
+}).listen(3000); 
